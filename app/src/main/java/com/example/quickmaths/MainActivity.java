1b97f18d925df0500e015bfa48e7ae1065b58020
Abdullah Rizwan <100567651+BOYYY743@users.noreply.github.com>
2022-08-28 19:04:24 +0500

    Logic changes.

--- app/src/main/java/com/example/quickmaths/MainActivity.java
+++ app/src/main/java/com/example/quickmaths/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.quickmaths;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    int random_1,random_2;
+    int answer;
+    boolean check;
+    static int score;
+
+EditText input;
+TextView number_1;
+    TextView number_2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +43,56 @@ public class MainActivity extends AppCompatActivity {
                 intent = new Intent(MainActivity.this, correct_answer.class);
                 startActivity(intent);
             }
-        });
+        });//
 
-        ImageButton closebtn1 = (ImageButton) findViewById(R.id.closebtn1);
 
-        closebtn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                Intent intent;
-                intent = new Intent(MainActivity.this, correct_answer.class);
-                startActivity(intent);
-            }
-        });
+
+initialize();
+set_randomNumbers();
+
+answer =getText(input);
+
+
+
+check();
+
+if (check==true)
+{
+    score +=10;
+}
+
+    }
+
+    public int getText(EditText input) {
+        return 0;
+    }
+
+    public void initialize()
+    {
+        input = findViewById(R.id.input);
+        number_1 = findViewById(R.id.gen_num_1);
+        number_2 = findViewById(R.id.gen_num_2);
+    }
+
+
+
+
+    public void set_randomNumbers()
+    {
+       random_1 = (int) (Math.random()*499);
+       random_2 = (int) (Math.random()*499);
+    }
+
+
+    public void check()
+    {
+        int sum = random_1+random_2;
+
+        if (answer == sum)
+        {
+            check = true;
+        }else{
+        check = false;
+        }
     }
 }
