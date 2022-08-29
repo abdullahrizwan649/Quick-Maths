b55295d18fc6de2214371fa476ab40ec03f5ce6b
Abdullah Rizwan <100567651+BOYYY743@users.noreply.github.com>
2022-08-29 12:45:10 +0500

    Logic changes.

--- app/src/main/java/com/example/quickmaths/MainActivity.java
+++ app/src/main/java/com/example/quickmaths/MainActivity.java
@@ -1,98 +1,107 @@
 package com.example.quickmaths;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
+
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Objects;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     int random_1,random_2;
     int answer;
     boolean check;
-    static int score;
+    int score;
 
 EditText input;
 TextView number_1;
-    TextView number_2;
-
+TextView number_2;
+TextView score_counter;
+ImageButton checkbtn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Objects.requireNonNull(getSupportActionBar()).hide();
-
-        ImageButton checkbtn1 = (ImageButton) findViewById(R.id.checkbtn1);
-
-        checkbtn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                Intent intent;
-                intent = new Intent(MainActivity.this, correct_answer.class);
-                startActivity(intent);
-            }
-        });//
-
-
-
-initialize();
-set_randomNumbers();
-
-answer =getText(input);
-
-
-
-check();
-
-if (check==true)
-{
-    score +=10;
-}
-
-    }
-
-    public int getText(EditText input) {
-        return 0;
-    }
-
-    public void initialize()
-    {
+       checkbtn1 = findViewById(R.id.checkbtn1);
         input = findViewById(R.id.input);
         number_1 = findViewById(R.id.gen_num_1);
         number_2 = findViewById(R.id.gen_num_2);
+        score_counter = findViewById(R.id.score_counter);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        set_randomNumbers();
+        number_1.setText(String.valueOf(random_1));
+        number_2.setText(String.valueOf(random_2));
+
+        checkbtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                run();
+              //  check();
+                Toast.makeText(MainActivity.this, "answer"+answer+"score"+ score , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
+    public void run() {
+        for (int i = 0; i < 5; i++) {
 
+            answer = Integer.parseInt(input.getText().toString());
+            check();
+
+            if (check == true) {
+                score = 10;
+            } else {
+                score +=0;
+            }
+            score_counter.setText(String.valueOf(score));
+        }
+    }
 
 
     public void set_randomNumbers()
-    {
-       random_1 = (int) (Math.random()*499);
-       random_2 = (int) (Math.random()*499);
-    }
-
-
-    public void check()
-    {
-        int sum = random_1+random_2;
-
-        if (answer == sum)
         {
-            check = true;
-        }else{
-        check = false;
+           random_1 = (int) (Math.random()*49);
+           random_2 = (int) (Math.random()*49);
+            Log.d("rd1", "set_randomNumbers: "+random_1);
+            Log.d("rd2", "set_randomNumbers: "+random_2);
         }
-    }
+
+
+        public void check()
+        {
+            int sum = random_1+random_2;
+
+            if (answer == sum){
+
+                check = true;
+                score += 10;
+            }
+            else{
+            check = false;
+            }
+            score_counter.setText(String.valueOf(score));
+        }
+
+
+
 }
+
+
